60620cd556353871d4382581b412c21ea7eacee5
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2022-09-02 15:17:33 +0200

    Thunk & dispatch types

--- src/reducers/store.tsx
+++ src/reducers/store.tsx
@@ -1,7 +1,8 @@
-import {tasksReducer} from './tasks-reducer';
-import {todolistsReducer} from './todolist-reducer';
+import {TasksActionType, tasksReducer} from './tasks-reducer';
+import {TodolistsActionType, todolistsReducer} from './todolist-reducer';
 import {applyMiddleware, combineReducers, legacy_createStore} from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import thunkMiddleware, {ThunkAction, ThunkDispatch} from 'redux-thunk';
+import {useDispatch} from "react-redux";
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -12,9 +13,15 @@ const rootReducer = combineReducers({
 console.log(typeof rootReducer)
 // непосредственно создаём store
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunkMiddleware));
+export const useAppDispatch = () => useDispatch<AppDispatch>()
 // определить автоматически тип всего объекта состояния
-export type AppRootStateType = ReturnType<typeof rootReducer>
-
+export type AppRootStateType = ReturnType<typeof rootReducer>;
+//все типа actions для вcего app
+export type AppActionType = TodolistsActionType | TasksActionType;
+//типизация Thunk
+export type AppThunkType<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AppActionType>;
+//типмзация Dispatch
+export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, AppActionType>
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store;
